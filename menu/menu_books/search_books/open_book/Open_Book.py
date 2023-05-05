e4eed8e14bd25246e435b7c6acf9f4e067c7375a
Dmitrii <73591672+Oberon1224@users.noreply.github.com>
2023-05-06 01:53:05 +0300

    Update Open_Book.py
    Reduction to standard format - removal of unnecessary spaces

--- menu/menu_books/search_books/open_book/Open_Book.py
+++ menu/menu_books/search_books/open_book/Open_Book.py
@@ -5,14 +5,14 @@ import sys
 sys.path.append(sys.path[0])
 
 class Open_Book:
-     def __init__(self,id_book):
-          f=open('settings.txt','r')
-          name_NDB=f.read()
-          f.close()
-          self.sqlite_connection=sqlite3.connect(name_NDB)
-          self.query="SELECT path FROM view_books WHERE id_book = ?"
-          cursor=self.sqlite_connection.cursor()
-          cursor.execute(self.query,(id_book,))
-          all_results=cursor.fetchone()
-          self.sqlite_connection.close()
-          os.system(all_results[0])
+    def __init__(self,id_book):
+        f=open('settings.txt','r')
+        name_NDB=f.read()
+        f.close()
+        self.sqlite_connection=sqlite3.connect(name_NDB)
+        self.query="SELECT path FROM view_books WHERE id_book = ?"
+        cursor=self.sqlite_connection.cursor()
+        cursor.execute(self.query,(id_book,))
+        all_results=cursor.fetchone()
+        self.sqlite_connection.close()
+        os.system(all_results[0])
