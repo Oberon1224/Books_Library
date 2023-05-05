fa4185d8116440e32185b28bb0753304a0571a96
Dmitrii <73591672+Oberon1224@users.noreply.github.com>
2023-05-06 01:45:11 +0300

    Update Update.py
    Reduction to standard format - removal of unnecessary spaces

--- sql_query/sql_update/Update.py
+++ sql_query/sql_update/Update.py
@@ -1,42 +1,42 @@
 import sqlite3
 #запрос изменения прогресса состояния чтения
 class update_state_progress:
-     def __init__(self,progress,id_book):
-          f=open('settings.txt','r')
-          name_NDB=f.read()
-          f.close()
-          self.sqlite_connection=sqlite3.connect(name_NDB)
-          query="""UPDATE state SET name = ?
-                    WHERE id_book= ?;"""
-          cursor = self.sqlite_connection.cursor()
-          cursor.execute(query,(progress,id_book))
-          self.sqlite_connection.commit()
-          self.sqlite_connection.close()
+    def __init__(self,progress,id_book):
+        f=open('settings.txt','r')
+        name_NDB=f.read()
+        f.close()
+        self.sqlite_connection=sqlite3.connect(name_NDB)
+        query="""UPDATE state SET name = ?
+                 WHERE id_book= ?;"""
+        cursor = self.sqlite_connection.cursor()
+        cursor.execute(query,(progress,id_book))
+        self.sqlite_connection.commit()
+        self.sqlite_connection.close()
           
 #запрос изменения страницы состояния чтения          
 class update_state_page:
-     def __init__(self,page,id_book):
-          f=open('settings.txt','r')
-          name_NDB=f.read()
-          f.close()
-          self.sqlite_connection=sqlite3.connect(name_NDB)
-          query="""UPDATE state SET page = ?
-                    WHERE id_book = ?;"""
-          cursor = self.sqlite_connection.cursor()
-          cursor.execute(query,(page,id_book))
-          self.sqlite_connection.commit()
-          self.sqlite_connection.close()
+    def __init__(self,page,id_book):
+        f=open('settings.txt','r')
+        name_NDB=f.read()
+        f.close()
+        self.sqlite_connection=sqlite3.connect(name_NDB)
+        query="""UPDATE state SET page = ?
+                 WHERE id_book = ?;"""
+        cursor = self.sqlite_connection.cursor()
+        cursor.execute(query,(page,id_book))
+        self.sqlite_connection.commit()
+        self.sqlite_connection.close()
           
 #запрос изменения пути к книге
 class update_about_path:
-     def __init__(self,path,id_book):
-          f=open('settings.txt','r')
-          name_NDB=f.read()
-          f.close()
-          self.sqlite_connection=sqlite3.connect(name_NDB)
-          query="""UPDATE about SET path = ?
-                    WHERE id_book = ?;"""
-          cursor = self.sqlite_connection.cursor()
-          cursor.execute(query,(path,id_book))
-          self.sqlite_connection.commit()
-          self.sqlite_connection.close()
+    def __init__(self,path,id_book):
+        f=open('settings.txt','r')
+        name_NDB=f.read()
+        f.close()
+        self.sqlite_connection=sqlite3.connect(name_NDB)
+        query="""UPDATE about SET path = ?
+                 WHERE id_book = ?;"""
+        cursor = self.sqlite_connection.cursor()
+        cursor.execute(query,(path,id_book))
+        self.sqlite_connection.commit()
+        self.sqlite_connection.close()
